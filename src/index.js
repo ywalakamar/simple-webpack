8ee2c4bd7651d7097b8ec4ca998ca48c417b8ce6
ywalakamar <barxwells@gmail.com>
2020-07-13 12:10:11 +0300

    chore(project): setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import _ from 'lodash'
+const component = () => {
+  const element = document.createElement("div");
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return element;
+};
+
+document.body.append(component());
