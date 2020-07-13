fa9f937e6fd155c8e87d1757e02024b08d88ee8f
ywalakamar <barxwells@gmail.com>
2020-07-13 14:11:51 +0300

    chore(images): add image

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.svg';
 
 const component = () => {
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add image to existing div
+  const icon = new Image();
+  icon.src = Icon;
+  element.appendChild(icon);
   return element;
 };
 
